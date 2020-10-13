f3902c879141b3241b783bbb7cd56e8b60ab75d1
matthijs <matthijs22@gmail.com>
2020-10-14 00:52:11 +0200

    Added base classes

---
+++ server.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+from model.model import Model
+
+import json
+
+
+app = Flask(__name__)
+api = Api(app)
+
+tm = Model()
+
+class Model(Resource):
+    def get(self):
+        return {"Model":tm.name,"Status": "Running"}, 200
+class Predict(Resource):
+    def get(self):
+        return "Post requests only"
+    def post(self):
+        data = request.json
+        try:
+            return tm.predict(data), 200
+        except ValueError:
+            return "ValueError: Inputshape incorrect"
+        else:
+            return "An error occurred"
+
+api.add_resource(Model, '/model')
+api.add_resource(Predict, '/predict')
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=8080)

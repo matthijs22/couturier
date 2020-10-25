5bba2e47fa17736c2f1f4c8e4fd93c384aebde27
matthijs <matthijs22@gmail.com>
2020-10-25 17:08:31 +0100

    Added example.py

---
+++ models/Example.py
@@ -0,0 +1,11 @@
+class Example():
+    def __init__(self):
+        self.name = "Example"
+        #self.model = pickle.load(open("./models/pickles/"+self.name+".model.sav","rb"))
+        #self.scaler = pickle.load(open("./models/pickles/"+self.name+".scaler.sav","rb"))
+    def __str__(self):
+        return self.name
+    def predict(self, data):
+        #data = self.scaler.transform(data)
+        #probabilities = self.model.predict_proba(data)
+        return  [[0,1] for x in range(len(data))]
